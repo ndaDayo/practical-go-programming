dffa068204cd9fca3cd5ff680f947d7de7151932
ndaDayo <hayato711+nda@gmail.com>
2023-10-11 22:12:58 +0900

    add: SlackWarning

--- ch04/warning/warning.go
+++ ch04/warning/warning.go
@@ -1,8 +1,14 @@
 package warning
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
+
+	"github.com/gen2brain/beeep"
 )
 
 type Warning interface {
@@ -13,4 +19,41 @@ type ConsoleWarning struct{}
 
 func (c ConsoleWarning) Show(message string) {
 	fmt.Fprintf(os.Stderr, "[%s]: %s", os.Args[0], message)
+}
+
+type DesktopWarning struct{}
+
+func (d DesktopWarning) Show(message string) {
+	beeep.Alert(os.Args[0], message, "")
+}
+
+type SlackWarning struct {
+	URL     string
+	Channel string
+}
+
+type slackMessage struct {
+	Text      string `json:"text"`
+	Username  string `json:"username"`
+	IconEmoji string `json:"icon_emoji"`
+	Channel   string `json:"channel"`
+}
+
+func (s SlackWarning) Show(message string) {
+	params, _ := json.Marshal(slackMessage{
+		Text:      message,
+		Username:  os.Args[0],
+		IconEmoji: ":robot_face:",
+		Channel:   s.Channel,
+	})
+
+	resp, err := http.PostForm(
+		s.URL,
+		url.Values{"payload": {string(params)}},
+	)
+
+	if err == nil {
+		io.ReadAll(resp.Body)
+		defer resp.Body.Close()
+	}
 }
