3b6a9b8990df17dad5407188d51534fb41bde1b9
ndaDayo <hayato711+nda@gmail.com>
2023-10-14 15:46:25 +0900

    add: handler

--- ch05/wrap/main.go
+++ ch05/wrap/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 type transientError struct {
@@ -10,6 +11,21 @@ type transientError struct {
 
 func (t transientError) Error() string {
 	return fmt.Sprintf("transient error: %v", t.err)
+}
+
+func GetTransactionAmountHandler(w http.ResponseWriter, r *http.Request) (float32, error) {
+	transactionID := r.URL.Query().Get("transaction")
+	amount, err := getTransactionAmount(transactionID)
+	if err != nil {
+		switch err := err.(type) {
+		case transientError:
+			http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		default:
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+	}
+
+	return amount, nil
 }
 
 func getTransactionAmount(transactionID string) (float32, error) {
